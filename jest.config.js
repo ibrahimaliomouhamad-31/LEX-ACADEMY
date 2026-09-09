aed32718bb14ff3a37330a2954cd4dfa8717e79c
apple <apple@apples-iMac.local>
2026-09-09 09:46:26 +0100

    chore: add Jest configuration for unit tests

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  transformIgnorePatterns: ['node_modules/(?!(@react-native|react-native|expo)/)'],
+};
